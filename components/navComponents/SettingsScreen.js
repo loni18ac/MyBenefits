e3ee85e8f45ccee8b5889ba955f2642df572a306
loni18ac <70534823+loni18ac@users.noreply.github.com>
2022-10-01 14:45:17 +0200

    final

--- components/navComponents/SettingsScreen.js
+++ components/navComponents/SettingsScreen.js
@@ -1,7 +1,7 @@
 import {Text, View } from 'react-native';
 import * as React from "react";
 
-//Denne side er ikke gennemarbejdet, da vi skal gentænke hvad en bruger skal kunne indstille. 
+//Denne side er ikke gennemarbejdet, da vi skal gentænke hvad en bruger skal kunne indstille
 export default function SettingsScreen() {
     return (
         <View>
